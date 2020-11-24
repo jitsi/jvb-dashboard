a17f08082dabecfc10852c76bba68ba821539fc3
Brian Baldino <bbaldino@gmail.com>
2020-11-23 16:25:14 -0800

    conference cleanup

--- src/main/kotlin/Conference.kt
+++ src/main/kotlin/Conference.kt
@@ -1,8 +1,13 @@
 import kotlinx.browser.window
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.await
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.css.paddingLeft
 import kotlinx.css.pct
@@ -19,39 +24,33 @@ import styled.css
 import styled.styledDiv
 
 class Conference : RComponent<ConferenceProps, ConferenceState>() {
-    private val epChannels = mutableMapOf<String, Channel<EndpointData>>()
+    private val epChannels: MutableMap<String, Channel<EndpointData>> = mutableMapOf()
+    private var job: Job? = null
+
     init {
         state.epIds = arrayOf()
         state.expanded = false
     }
-    override fun ConferenceState.init() {
-        GlobalScope.launch {
-            try {
-                while (true) {
-                    val jvbData = fetchData()
-                    val now = jvbData.time as Number
-                    val confData = jvbData.conferences[props.id]
-                    val epIds = getEpIds(confData)
-                    setState {
-                        this.name = confData.name.unsafeCast<String>().substringBefore('@')
-                        this.epIds = epIds
-                    }
-                    epChannels.forEach { (epId, epChannel) ->
-                        val epData = confData.endpoints[epId]
-                        epChannel.send(EndpointData(now, epData))
-                    }
-                    delay(1000)
-                }
-            } catch (t: Throwable) {
-                console.log("exception: ", t)
-            }
+
+    override fun componentDidMount() {
+        job = GlobalScope.launch { fetchDataLoop() }
+    }
+
+    override fun componentWillUnmount() {
+        job?.cancel("Unmounting")
+        epChannels.forEach { (_, channel) ->
+            channel.close()
         }
     }
 
+    // TODO: we need this function because react doesn't do a deep comparison on the epIds array to know whether
+    // or not it changed.  We could introduce an 'EndpointList' component whose only state was the IDs, and then
+    // get rid of this override here and move it there (which would be a bit cleaner, since here we may add more state
+    // and would have to remember to take it into account in this method)
     override fun shouldComponentUpdate(nextProps: ConferenceProps, nextState: ConferenceState): Boolean {
         return (state.expanded != nextState.expanded) ||
-                (state.name != nextState.name) ||
-                (!state.epIds.contentEquals(nextState.epIds))
+            (state.name != nextState.name) ||
+            (!state.epIds.contentEquals(nextState.epIds))
     }
 
     override fun RBuilder.render() {
@@ -71,7 +70,6 @@ class Conference : RComponent<ConferenceProps, ConferenceState>() {
                 }
                 attrs {
                     onClickFunction = { _ ->
-                        console.log("toggling expand")
                         setState {
                             expanded = !expanded
                         }
@@ -104,11 +102,33 @@ class Conference : RComponent<ConferenceProps, ConferenceState>() {
         }
     }
 
-    private suspend fun fetchData(): dynamic {
-        return window.fetch("${props.baseUrl}/${props.id}")
-            .await()
-            .json()
-            .await()
+    private suspend fun CoroutineScope.fetchDataLoop() {
+        while (isActive) {
+            try {
+                val jvbData = window.fetch("${props.baseUrl}/${props.id}")
+                    .await()
+                    .json()
+                    .await()
+                    .asDynamic()
+                val now = jvbData.time.unsafeCast<Number>()
+                val confData = jvbData.conferences[props.id]
+                val epIds = getEpIds(confData)
+                setState {
+                    this.name = confData.name.unsafeCast<String>().substringBefore('@')
+                    this.epIds = epIds
+                }
+                epChannels.forEach { (epId, epChannel) ->
+                    val epData = confData.endpoints[epId]
+                    epChannel.send(EndpointData(now, epData))
+                }
+                delay(1000)
+            } catch (c: CancellationException) {
+                console.log("Conference ${props.id} fetch loop cancelled")
+                throw c
+            } catch (t: Throwable) {
+                console.log("Conference ${props.id} fetch loop error: ", t)
+            }
+        }
     }
 }
 
